a4442f89011bec3facadb55d21443a841c4f5d7d
raganapaula <aragievicz@gmail.com>
2022-12-16 18:40:21 -0300

    Atualização para Fazer a paginação

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -1,5 +1,8 @@
 using Biblioteca.Models;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
 
 namespace Biblioteca.Controllers
 {
@@ -14,6 +17,8 @@ namespace Biblioteca.Controllers
         [HttpPost]
         public IActionResult Cadastro(Livro l)
         {
+            if(!string.IsNullOrEmpty(l.Titulo) && !string.IsNullOrEmpty(l.Autor) && l.Ano!=0)
+            {
             LivroService livroService = new LivroService();
 
             if(l.Id == 0)
@@ -26,9 +31,15 @@ namespace Biblioteca.Controllers
             }
 
             return RedirectToAction("Listagem");
+            }
+            else
+            {
+                ViewData["mensagem"]="Preencha todos os campos";
+                return View();
+            }
         }
 
-        public IActionResult Listagem(string tipoFiltro, string filtro)
+        public IActionResult Listagem(string tipoFiltro, string filtro, string itensPorPagina, int NumDaPagina, int PaginaAtual)
         {
             Autenticacao.CheckLogin(this);
             FiltrosLivros objFiltro = null;
@@ -38,6 +49,10 @@ namespace Biblioteca.Controllers
                 objFiltro.Filtro = filtro;
                 objFiltro.TipoFiltro = tipoFiltro;
             }
+
+                ViewData["livrosPorPagina"] = (string.IsNullOrEmpty(itensPorPagina) ? 10 : Int32.Parse(itensPorPagina));
+                ViewData["PaginaAtual"] = (PaginaAtual !=0 ? PaginaAtual : 1);
+                
             LivroService livroService = new LivroService();
             return View(livroService.ListarTodos(objFiltro));
         }
